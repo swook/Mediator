768609601debb76fc090bbfa21668cd56bc1c308
Seon-Wook Park <seon.wook@swook.net>
2013-01-13 16:05:50 +0000

    Edit version and date for release

--- class.mediator.plugin.php
+++ class.mediator.plugin.php
@@ -5,22 +5,20 @@
 $PluginInfo['Mediator'] = array(
 	'Name' => 'Mediator',
 	'Description' => 'Allows automatic embedding of media by converting media links to embed code. Supports Youtube, Vimeo, Imgur, Pastebin, Soundcloud [Bandcamp].',
-	'Version' => '0.1.42',
-	'Date' => '12 Nov 2012',
+	'Version' => '0.2',
+	'Date' => '13 Jan 2012',
 	'Author' => 'Seon-Wook Park',
 	'AuthorEmail' => 'seon.wook@swook.net',
 	'AuthorUrl' => 'http://www.swook.net/',
-	'RequiredApplications' => False,
-	'RequiredTheme' => False, 
-	'RequiredPlugins' => False,
-	'RegisterPermissions' => False,
-	'SettingsPermission' => False,
+	'RequiredTheme' => FALSE, 
+	'RequiredPlugins' => FALSE,
+	'RegisterPermissions' => FALSE,
+	'SettingsPermission' => FALSE,
 	'License' => 'CC BY-NC-SA'
 );
 
 class MediatorPlugin implements Gdn_IPlugin {
-	
-	//public function DiscussionController_Render_Before($Sender) {
+
 	public function Base_Render_Before(&$Sender) {
 		if ($Sender->DeliveryType() == DELIVERY_TYPE_ALL && $Sender->SyndicationMethod == SYNDICATION_NONE) {
 			$Sender->AddJsFile('plugins/Mediator/mediator.js');
