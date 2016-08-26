4ef31ee58be7c4b187ed00fda37518719bd55509
Seon-Wook Park <seon.wook@swook.net>
2016-08-26 11:25:47 +0200

    Re-license with GPLv3

--- class.mediator.plugin.php
+++ class.mediator.plugin.php
@@ -1,11 +1,25 @@
 <?php if (!defined('APPLICATION')) exit();
+/*
+ This file is part of Mediator
 
-/* Mediator Plugin for Vanilla Forums by Seon-Wook Park | CC BY-NC-SA */
+ Mediator is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Foobar is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Mediator. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 $PluginInfo['Mediator'] = array(
 	'Name' => 'Mediator',
 	'Description' => 'Allows automatic embedding of media by converting media links to embed code. Supports Youtube, Vimeo, Imgur, Pastebin, Soundcloud [Bandcamp], Gyazo.',
-	'Version' => '0.2.16',
+	'Version' => '0.2.17',
 	'Date' => '01 Feb 2012',
 	'Author' => 'Seon-Wook Park',
 	'AuthorEmail' => 'seon.wook@swook.net',
@@ -14,7 +28,7 @@ $PluginInfo['Mediator'] = array(
 	'RequiredPlugins' => FALSE,
 	'RegisterPermissions' => FALSE,
 	'SettingsPermission' => FALSE,
-	'License' => 'CC BY-NC-SA'
+	'License' => 'GNU GPL3'
 );
 
 class MediatorPlugin implements Gdn_IPlugin {
